0cbcb097409da28120d8b10c3912006786aedc10
Tonatiu Cruz Bautista <tonatiu_98x@hotmail.com>
2026-06-03 10:25:37 -0600

    Add validation to PersonalInfo model and implement validation in PersonalInfoServiceImpl

--- src/main/java/com/portfolio/my_portfolio_backend/service/PersonalInfoServiceImpl.java
+++ src/main/java/com/portfolio/my_portfolio_backend/service/PersonalInfoServiceImpl.java
@@ -4,6 +4,9 @@ import com.portfolio.my_portfolio_backend.model.PersonalInfo;
 import com.portfolio.my_portfolio_backend.repository.IPersonalInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,9 +15,17 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class PersonalInfoServiceImpl implements IPersonalInfoService {
     private final IPersonalInfoRepository  personalInfoRepository;
+    private final Validator validator;
 
     @Override
     public PersonalInfo save(PersonalInfo personalInfo) {
+        BindingResult bindingResult = new BeanPropertyBindingResult(personalInfo, "personalInfo");
+        validator.validate(personalInfo, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return null;
+        }
+
         return personalInfoRepository.save(personalInfo);
     }
 
